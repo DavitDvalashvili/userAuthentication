8ce51711b2c0d696e589609800a069eea4546a2a
davit dvalashvili <davitdvalashvili1996@gmail.com>
2023-11-29 23:07:31 +0400

    wip: work on loign page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,31 @@
+import { Link } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="form_container">
+      <h2>Login Account</h2>
+      <form>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" placeholder="Enter your email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+          />
+        </div>
+        <button type="submit">Submit</button>
+        <span>
+          Already have an account? <Link to={"/signup"}>Signup</Link>
+        </span>
+      </form>
+      <ToastContainer />
+    </div>
+  );
 };
 
 export default Login;
--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,7 +1,7 @@
 //import { useState } from "react";
 import { Link } from "react-router-dom";
 //import axios from "axios";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 
 const Signup = () => {
   return (
